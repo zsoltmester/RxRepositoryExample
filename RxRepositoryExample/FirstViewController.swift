ad9f26cbbbb6e1070a784eb4397ec564a6cf2c00
zsmester <zs.mester@mito.hu>
2018-08-23 17:05:04 +0200

    Initialize the project.

---
+++ RxRepositoryExample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RxRepositoryExample
+//
+//  Created by Mester Zsolt on 2018. 08. 23..
+//  Copyright © 2018. Mester Zsolt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
